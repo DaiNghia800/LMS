0dfe56b6a01d3aa9f8f5ab75b984609060c3fc1b
Bui Dai Nghia <6451071050@st.utc2.edu.vn>
2025-11-25 01:19:47 +0700

    ReFix build errors by ignoring lint

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,10 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // 2. Bỏ qua lỗi ESLint khi build
   eslint: {
     ignoreDuringBuilds: true,
   },
   serverExternalPackages: ["@uploadthing/mime-types"],
-} as any; 
+};
 
 export default nextConfig;
